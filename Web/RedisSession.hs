65bab94b3885a58adfb9444c07f8d45db1ac02de
Rehno Lindeque <errantkid@gmail.com>
2014-03-21 23:07:59 +0200

    Work-in-progress update of the code to Yesod and Hedis

--- Web/RedisSession.hs
+++ Web/RedisSession.hs
@@ -1,13 +1,13 @@
 module Web.RedisSession (
-		makeRedisConnectionPool,
-		makeRedisLocalConnectionPool,
-		setSession, setSessionExpiring,
-		getSession,
-		newKey,
-		Redis
-	) where
+    makeRedisConnectionPool,
+    makeRedisLocalConnectionPool,
+    setSession, setSessionExpiring,
+    getSession,
+    newKey,
+    Redis
+  ) where
 
-import Database.Redis.Redis
+import Database.Redis
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Data.ByteString.Lazy (toChunks)
@@ -16,33 +16,34 @@ import Data.Conduit.Pool
 import System.Entropy (getEntropy)
 import Data.Digest.Pure.MD5 (md5)
 import Data.Time (getCurrentTime)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
 
-makeRedisConnectionPool :: String -> String -> IO (Pool Redis)
-makeRedisConnectionPool server port = createPool (connect server port) disconnect 1 0.5 1
+----makeRedisConnectionPool :: ConnectInfo -> IO Connection
+----makeRedisConnectionPool cinfo = connect cinfo
 
-makeRedisLocalConnectionPool :: IO (Pool Redis)
-makeRedisLocalConnectionPool = makeRedisConnectionPool localhost defaultPort
+----makeRedisLocalConnectionPool :: IO (Pool Connection)
+----makeRedisLocalConnectionPool = makeRedisConnectionPool localhost defaultPort
 
-setSession :: (Binary a) => Pool Redis -> ByteString -> a -> IO ()
-setSession pool key value = withResource pool $ \redis -> do
-	set redis key $ encode value
-	return ()
+--setSession :: (Binary a) => Connection -> ByteString -> a -> IO ()
+--setSession conn key value = runRedis conn $ do
+--    set key value
+--    return ()
 
-setSessionExpiring :: (Binary a) => Pool Redis -> ByteString -> a -> Int -> IO ()
-setSessionExpiring pool key value timeout = withResource pool $ \redis -> do
-	setEx redis key timeout $ encode value
-	return ()
+--setSessionExpiring :: (Binary a) => Connection -> ByteString -> a -> Int -> IO ()
+--setSessionExpiring conn key value timeout = runRedis conn $ do
+--    setex key timeout $ encode value
+--    return ()
 
-getSession :: (Binary a) => Pool Redis -> ByteString -> IO (Maybe a)
-getSession pool key = withResource pool $ \redis -> do
-	val <- get redis key
-	case val of
-		RBulk Nothing -> return Nothing
-		RBulk (Just v) -> return $ Just $ decode v
-		_ -> return Nothing
+--getSession :: (Binary a) => Connection -> ByteString -> IO (Either Text a)
+--getSession conn key = runRedis conn $ do
+--    val <- get key
+--    case val of
+--        Left (Error e) -> Left $ decodeUtf8 e
+--        _              -> val
 
-newKey :: IO ByteString
-newKey = do
-	ent <- getEntropy 80
-	now <- getCurrentTime
-	return $ B.concat [(B.concat . toChunks . encode . md5 . encode . show) now, ent]
+--newKey :: IO ByteString
+--newKey = do
+--    ent <- getEntropy 80
+--    now <- getCurrentTime
+--    return $ B.concat [(B.concat . toChunks . encode . md5 . encode . show) now, ent]
--- Yesod/Session/Redis.hs
+++ Yesod/Session/Redis.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 module Yesod.Session.Redis (
-		localRedisSessionBackend,
-		redisSessionBackend
-	) where
+    --localRedisSessionBackend,
+    --redisSessionBackend
+    loadRedisSession
+  ) where
 
-import qualified Web.RedisSession as R
+import qualified Database.Redis as R
+--import qualified Web.RedisSession as R
 import Yesod.Core
+import Yesod.Core.Types
 import qualified Network.Wai as W
 import Web.Cookie
 import Control.Monad.Trans (liftIO)
@@ -16,53 +19,97 @@ import Data.Binary
 import Data.Text (Text)
 import Data.Text.Encoding
 import Control.Monad (liftM)
-
-instance Binary Text where 
-	put = put . encodeUtf8 
-	get = liftM decodeUtf8 get
-
-sessionName = "yesodSession"
-
-loadRedisSession :: (Yesod master) => Pool R.Redis -> master -> W.Request -> UTCTime -> IO BackendSession
-loadRedisSession pool _ req now = do
-	let val = do
-		raw <- lookup "Cookie" $ W.requestHeaders req
-		lookup sessionName $ parseCookies raw
-	case val of
-		Nothing -> return []
-		Just s -> fmap (fromMaybe []) $ liftIO $ R.getSession pool s
-
-saveRedisSession :: (Yesod master) => Pool R.Redis -> Int -> master -> W.Request -> UTCTime -> BackendSession -> BackendSession -> IO [Header]
-saveRedisSession pool timeout master req now _ sess = do
-	let val = do
-		raw <- lookup "Cookie" $ W.requestHeaders req
-		lookup sessionName $ parseCookies raw
-	key <- case val of
-		Nothing -> R.newKey
-		Just k -> return k
-	R.setSessionExpiring pool key sess timeout
-	return [AddCookie def {
-			setCookieName = sessionName,
-			setCookieValue = key,
-			setCookiePath = Just $ cookiePath master,
-			setCookieExpires = Just expires,
-			setCookieDomain = cookieDomain master,
-			setCookieHttpOnly = True
-		}]
-	where
-		expires = fromIntegral (timeout * 60) `addUTCTime` now
+import Control.Exception
+import Data.Typeable
+import Data.Time (getCurrentTime)
+import Data.Map (fromList, toList)
+import qualified Data.Map as M
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.ByteString.Lazy (toChunks)
+import Data.Digest.Pure.MD5 (md5)
+import System.Entropy (getEntropy)
 
 
-localRedisSessionBackend :: (Yesod master) => Int -> IO (SessionBackend master)
-localRedisSessionBackend = sessionBackend R.makeRedisLocalConnectionPool
+--instance Binary Text where 
+--    put = put . encodeUtf8 
+--    get = liftM decodeUtf8 get
 
-redisSessionBackend :: (Yesod master) => String -> String -> Int -> IO (SessionBackend master)
-redisSessionBackend server port = sessionBackend (R.makeRedisConnectionPool server port)
+data RedisSessionException = RedisSessionNotFound Text | RedisSessionCookieSessionNotFound
+    deriving (Show, Typeable)
+instance Exception RedisSessionException
 
-sessionBackend :: (Yesod master) => IO (Pool R.Redis) -> Int -> IO (SessionBackend master)
-sessionBackend mkPool timeout = do
-	pool <- mkPool
-	return $ SessionBackend {
-		sbSaveSession = saveRedisSession pool timeout,
-		sbLoadSession = loadRedisSession pool
-	}
+loadRedisSession :: R.Connection 
+                 -> ByteString    -- ^ Redis session key
+                 -> ByteString    -- ^ session name
+                 -> Integer       -- ^ time out
+                 -> W.Request 
+                 -> IO (SessionMap, SaveSession)
+loadRedisSession conn redisKey sessionName timeout req = do
+    let cookieVal = do
+          raw <- lookup "Cookie" $ W.requestHeaders req
+          lookup sessionName $ parseCookies raw
+    expires <- fmap addTimeout getCurrentTime
+    case cookieVal of
+        Nothing -> return $ (M.empty, saveRedisSession expires)
+        Just s -> R.runRedis conn $ do
+            redisHash <- R.hgetall s
+            case redisHash of
+                Left (R.Error e) -> throw $ RedisSessionNotFound $ decodeUtf8 e
+                Right hash       -> return (fromList $ mapFst decodeUtf8 hash, saveRedisSession expires)
+    where
+        saveRedisSession :: UTCTime -> SaveSession
+        saveRedisSession expires sessionMap = do
+            --R.setex conn key sess timeout
+            R.runRedis conn $ R.hmset redisKey $ mapFst encodeUtf8 $ toList sessionMap
+            return [AddCookie def 
+                { setCookieName = sessionName
+                , setCookieValue = redisKey
+                , setCookiePath = Just "/"
+                , setCookieExpires = Just expires
+                , setCookieDomain = Nothing
+                , setCookieHttpOnly = True
+                }]
+        mapFst f       = map $ \(a,b) -> (f a, b)
+        addTimeout now = fromIntegral (timeout * 60) `addUTCTime` now
+
+--newKey :: UTCTime -> IO ByteString
+--newKey now = do
+--    ent <- getEntropy 80
+--    return $ B.concat [(B.concat . toChunks . encode . md5 . encode . show) now, ent]
+
+        
+
+--fmap (fromMaybe []) $ liftIO $ R.getSession conn s
+
+--saveRedisSession :: (Yesod master) => R.Connection -> Int -> master -> W.Request -> UTCTime -> BackendSession -> BackendSession -> IO [Header]
+--saveRedisSession conn timeout master req now _ sess = do
+--  let val = do
+--      raw <- lookup "Cookie" $ W.requestHeaders req
+--      lookup sessionName $ parseCookies raw
+--  key <- case val of
+--      Nothing -> R.newKey
+--      Just k -> return k
+--  R.setex conn key sess timeout
+--  return [AddCookie def {
+--          setCookieName = sessionName,
+--          setCookieValue = key,
+--          setCookiePath = Just $ cookiePath master,
+--          setCookieExpires = Just expires,
+--          setCookieDomain = cookieDomain master,
+--          setCookieHttpOnly = True
+--      }]
+--  where
+--      expires = fromIntegral (timeout * 60) `addUTCTime` now
+
+
+--localRedisSessionBackend :: (Yesod master) => Int -> IO (SessionBackend master)
+--localRedisSessionBackend = sessionBackend R.makeRedisLocalConnectionPool
+
+--redisSessionBackend :: (Yesod master) => String -> String -> Int -> IO (SessionBackend master)
+--redisSessionBackend server port = sessionBackend (R.makeRedisConnectionPool server port)
+
+--sessionBackend :: (Yesod master) => R.Connection -> Int -> IO (SessionBackend master)
+--sessionBackend conn timeout = return $ SessionBackend {
+--        sbLoadSession = loadRedisSession conn
+--    }
